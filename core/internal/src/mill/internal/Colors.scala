65765016fc4b64d869ea3d0ddc175fde282622c6
Li Haoyi <haoyi.sg@gmail.com>
2026-01-02 16:25:26 +0800

    minimize

--- core/internal/src/mill/internal/Colors.scala
+++
@@ -1,7 +0,0 @@
-package mill.internal
-
-case class Colors(info: fansi.Attrs, warn: fansi.Attrs, error: fansi.Attrs)
-object Colors {
-  object Default extends Colors(fansi.Color.Blue, fansi.Color.Yellow, fansi.Color.Red)
-  object BlackWhite extends Colors(fansi.Attrs.Empty, fansi.Attrs.Empty, fansi.Attrs.Empty)
-}
